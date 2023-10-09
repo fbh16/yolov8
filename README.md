c511074b9d61b77c59d5ed39882b9695bee02372
fbh16 <279144328@qq.com>
2023-10-09 15:45:18 +0800

    correct an error of Boundingbox

---
+++ README.md
@@ -0,0 +1,64 @@
+# Using YOLOv8 in ROS melodic
+
+## Environment
+**python3.6.9**    
+**torch-1.7.1+cu110**
+
+## How to run
+<p style="text-align: justify;">
+
+**1.** Download the code
+
+    git clone https://github.com/fbh16/yolov8_ros.git  
+    cd yolov8_ros/src/yolov8_ros
+    git clone https://github.com/ultralytics/ultralytics.git
+    cd .. && catkin_make
+    source devel/setup.bash
+
+<<<<<<< HEAD
+**2.** Modify the parameters in the **launch file**, such as the **model path**, **ROS topics**, **visualization options**, to suit your own requirements. 
+=======
+**2.** Modify the parameters in the **launch file**, such as the **model path**, **ROS topics**, **visualization options**, to suit your own requirements, download the YOLOv8 pretrained model at <https://github.com/ultralytics/assets/releases>. 
+>>>>>>> 8a122b34771d105968348025fda907ff43cf6fcc
+
+**3.** 
+If you are using a **Python version lower than 3.8**, modify the code at **line 76** of the file(**ultralytics/ultralytics/hub/auth.py**):  
+
+    if header := self.get_auth_header():  
+
+to:  
+
+    header = self.get_auth_header()
+    if header:
+
+**3.** Start the camera node or the rosbag, check the topics, and run the launch file.
+
+    roslaunch yolov8_ros v8.launch
+    
+</p>
+
+## Parameters
+<p style="text-align: justify;">
+
+**device:** Type 'cpu' if you are using CPU, and 'cuda' if you are using GPU.When you need to use CPU, you'll need to install a Python package with:   
+`python3 -m pip install py-cpuinfo`
+
+**model:** Enter the absolute path to the YOLOv8 model on your local machine.
+
+**class_yaml:** Please provide the specific path to the YAML file associated with your dataset. The model can obtain the classes of the dataset from this YAML file.
+
+**view_image:** Whether to show detection screen.
+
+**hide_label:** Whether to hide the BoundingBox label.
+
+**sub&pub:** ROS publish and subscribe topics.
+
+**single_class:** When you want to detect all classes, you don't need to input anything. When you want to display only a specific class, enter the name of that class, i.e., 'person'.  
+
+Some parameters, such as input image size and intersection over union (IoU), have default values set but can be modified as needed.
+
+<<<<<<< HEAD
+</p>
+=======
+</p>
+>>>>>>> 8a122b34771d105968348025fda907ff43cf6fcc
---
+++ src/detect_ros.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python3
+import yaml
+import rospy
+import cv2
+import sys
+import numpy as np
+import torch
+from pathlib import Path
+from cv_bridge import CvBridge
+from typing import Tuple, Union, List
+from sensor_msgs.msg import Image, CompressedImage
+from darknet_ros_msgs.msg import BoundingBox, BoundingBoxes
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1] / "ultralytics"
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+
+from ultralytics.nn.autobackend import AutoBackend
+from ultralytics.utils import ops
+from ultralytics.utils.checks import check_imgsz
+from ultralytics.data.augment import LetterBox
+from ultralytics.utils.torch_utils import select_device
+
+
+
+class YOLOv8Detector:
+    def __init__(self, 
+                 model: str, 
+                 device: str,
+                 data: str, 
+                 view_image: bool,
+                 hide_label: bool, 
+                 img_topic: str, 
+                 pub_topic1: str,
+                 pub_topic2: str, 
+                 single_cls: Union[str, None],
+                 imgsz: Union[Tuple[int, int], None] = (480, 640), ## h,w
+                 conf_thresh: float = 0.5, 
+                 iou_thresh: float = 0.45,
+                 done_warmup: bool = False,
+        ):
+        with open(data, 'r') as f:
+            yml_content = yaml.load(f, Loader=yaml.FullLoader)
+        
+        self.cls_dict = yml_content['names']
+        self.color_palette = np.random.uniform(0, 255, size=(len(self.cls_dict), 3))
+
+        if single_cls != "":
+            single_cls_id = [key for key, value in self.cls_dict.items() if value == single_cls][0]
+            self.single_cls = [int(single_cls_id)]
+        else:
+            self.single_cls = None
+        self.model = model
+        self.device = device
+        self.iou = iou_thresh
+        self.conf = conf_thresh
+        self.view_img = view_image
+        self.hide_lbl = hide_label
+        self.bridge = CvBridge()
+        
+        self.model = AutoBackend(model, device=select_device(self.device),
+                                 dnn=True, data=data, fp16=False, fuse=True)
+        
+        self.device = self.model.device  # update device
+        self.half = self.model.fp16  # update half
+        self.model.eval()
+
+        self.imgsz = check_imgsz(imgsz, stride=self.model.stride, min_dim=2)
+        
+        if not done_warmup:
+            self.model.warmup(imgsz=(1 if self.model.pt or self.model.triton else self.dataset.bs, 3, *self.imgsz))
+            done_warmup = True
+
+        self.img_sub = rospy.Subscriber(img_topic, Image, self.callback)
+        self.det_pub = rospy.Publisher(pub_topic1, BoundingBoxes, queue_size=1)
+        self.img_pub = rospy.Publisher(pub_topic2, Image, queue_size=1)
+        print("YOLOv8 is ready")
+
+    def tran2rawsz(self, det, imw, imh):
+        Kw = imw / 640
+        Kh = imh / 480
+        det[:,0] = det[:,0] * Kw
+        det[:,1] = det[:,1] * Kh
+        det[:,2] = det[:,2] * Kw
+        det[:,3] = det[:,3] * Kh
+        return det
+
+    def preprocess(self, im):
+        im0 = im.copy()
+        im = cv2.resize(im,(640, 480))
+        im = np.stack([LetterBox(self.imgsz, auto=self.model.pt, stride=self.model.stride)(image=im)])
+        im = im[..., ::-1].transpose((0, 3, 1, 2))  # BGR to RGB, BHWC to BCHW, (n, 3, h, w)
+        im = np.ascontiguousarray(im)  # contiguous
+        im = torch.from_numpy(im)
+        img = im.to(self.device)
+        img = img.half() if self.model.fp16 else img.float()  # uint8 to fp16/32
+        img /= 255  # 0~255 to 0.0~1.0
+        return img, im0
+        
+    def callback(self, stream):
+        self.img_pub.publish(stream)
+        im = self.bridge.imgmsg_to_cv2(stream, "bgr8")
+        img, im0 = self.preprocess(im)
+        img = img[None] if len(img.shape) == 3 else img
+        preds = self.model(img, augment=False, visualize=False)
+        preds = ops.non_max_suppression(
+            preds,
+            self.conf,
+            self.iou,
+            classes=self.single_cls,    
+            agnostic=False,
+            max_det=100,
+            nc=len(self.model.names), ## number of classes
+        )
+        det = preds[0].cpu().numpy()
+        bbs = BoundingBoxes()
+        bbs.header = stream.header
+        bbs.image_header = stream.header
+
+        if len(det):
+            # Rescale boxes from imgsz to im0's size
+            det[:, :4] = self.tran2rawsz(det[:,:4], int(im.shape[1]), int(im.shape[0]))
+            for *xyxy, conf, cls in reversed(det):
+                bb = BoundingBox()
+                cls_id = int(cls)
+                bb.Class = self.cls_dict[cls_id] ## clsid to cls name
+                bb.probability = conf 
+                bb.xmin = int(xyxy[0]) 
+                bb.ymin = int(xyxy[1])
+                bb.xmax = int(xyxy[2])
+                bb.ymax = int(xyxy[3])
+                bbs.bounding_boxes.append(bb)
+
+                if self.view_img:  
+                    color = self.color_palette[cls_id]   
+                    ## draw box label
+                    if self.hide_lbl:
+                        label = False
+                    else:
+                        label = f"{self.cls_dict[cls_id]} {int(conf*100)}%" 
+                        (label_width, label_height), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+                        label_x = bb.xmin
+                        label_y = bb.ymin - 10 if bb.ymin - 10 > label_height else bb.ymin + 10
+                        cv2.rectangle(im0, (label_x, label_y - label_height), (label_x + label_width, label_y + label_height), color, cv2.FILLED)
+                        cv2.putText(im0, label, (label_x+3, label_y+3), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+                    ## draw bounding box
+                    cv2.rectangle(im0, (bb.xmin, bb.ymin), (bb.xmax, bb.ymax), color, 2)   
+                    
+        self.det_pub.publish(bbs) ## bbs are based on original images' size
+
+        if self.view_img:
+            cv2.imshow('ALeafBoatOnTheHorizon', im0)
+            cv2.waitKey(1)  
+    
+
+if __name__ == '__main__':
+    
+    rospy.init_node('yolov8_ros')
+
+    detector = YOLOv8Detector(
+        model = rospy.get_param("~model"),
+        device = rospy.get_param("~device"),
+        data = rospy.get_param("~class_yaml"),
+        view_image = rospy.get_param("~view_image"),
+        hide_label = rospy.get_param("~hide_label"),
+        img_topic = rospy.get_param("~sub"),
+        pub_topic1 = rospy.get_param("~pub1"),
+        pub_topic2 = rospy.get_param("~pub2"),
+        single_cls= rospy.get_param("~single_class") ## A list of class indices to consider. If None, all classes will be considered.
+    )
+    
+    rospy.spin()
